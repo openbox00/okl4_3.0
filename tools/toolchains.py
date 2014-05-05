092483411bbf45c55dbc45596d75459c94ac4728
openbox00 <openbox00.tw@gmail.com>
2014-05-05 23:30:35 +0800

    change compiler's path

--- tools/toolchains.py
+++ tools/toolchains.py
@@ -1,60 +1,3 @@
-##############################################################################
-# Copyright (c) 2007 Open Kernel Labs, Inc. (Copyright Holder).
-# All rights reserved.
-# 
-# 1. Redistribution and use of OKL4 (Software) in source and binary
-# forms, with or without modification, are permitted provided that the
-# following conditions are met:
-# 
-#     (a) Redistributions of source code must retain this clause 1
-#         (including paragraphs (a), (b) and (c)), clause 2 and clause 3
-#         (Licence Terms) and the above copyright notice.
-# 
-#     (b) Redistributions in binary form must reproduce the above
-#         copyright notice and the Licence Terms in the documentation and/or
-#         other materials provided with the distribution.
-# 
-#     (c) Redistributions in any form must be accompanied by information on
-#         how to obtain complete source code for:
-#        (i) the Software; and
-#        (ii) all accompanying software that uses (or is intended to
-#        use) the Software whether directly or indirectly.  Such source
-#        code must:
-#        (iii) either be included in the distribution or be available
-#        for no more than the cost of distribution plus a nominal fee;
-#        and
-#        (iv) be licensed by each relevant holder of copyright under
-#        either the Licence Terms (with an appropriate copyright notice)
-#        or the terms of a licence which is approved by the Open Source
-#        Initative.  For an executable file, "complete source code"
-#        means the source code for all modules it contains and includes
-#        associated build and other files reasonably required to produce
-#        the executable.
-# 
-# 2. THIS SOFTWARE IS PROVIDED ``AS IS'' AND, TO THE EXTENT PERMITTED BY
-# LAW, ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-# PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED.  WHERE ANY WARRANTY IS
-# IMPLIED AND IS PREVENTED BY LAW FROM BEING DISCLAIMED THEN TO THE
-# EXTENT PERMISSIBLE BY LAW: (A) THE WARRANTY IS READ DOWN IN FAVOUR OF
-# THE COPYRIGHT HOLDER (AND, IN THE CASE OF A PARTICIPANT, THAT
-# PARTICIPANT) AND (B) ANY LIMITATIONS PERMITTED BY LAW (INCLUDING AS TO
-# THE EXTENT OF THE WARRANTY AND THE REMEDIES AVAILABLE IN THE EVENT OF
-# BREACH) ARE DEEMED PART OF THIS LICENCE IN A FORM MOST FAVOURABLE TO
-# THE COPYRIGHT HOLDER (AND, IN THE CASE OF A PARTICIPANT, THAT
-# PARTICIPANT). IN THE LICENCE TERMS, "PARTICIPANT" INCLUDES EVERY
-# PERSON WHO HAS CONTRIBUTED TO THE SOFTWARE OR WHO HAS BEEN INVOLVED IN
-# THE DISTRIBUTION OR DISSEMINATION OF THE SOFTWARE.
-# 
-# 3. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ANY OTHER PARTICIPANT BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 import copy
 import os
 import sys
@@ -404,12 +347,20 @@ class macho_ia32_toolchain(generic_gcc):
 
 
 #gnu_arm_libgcc_toolchain = generic_gcc("arm-linux-")
-gnu_arm_eabi_toolchain = generic_gcc_3_4_libgcc("/opt/okl/Linux-i386/arm/gcc-4.2.4-glibc-2.7/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-")
+#gnu_arm_eabi_toolchain = generic_gcc_3_4_libgcc("/opt/okl/Linux-i386/arm/gcc-4.2.4-glibc-2.7/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-")
+gnu_arm_eabi_toolchain = generic_gcc_3_4_libgcc("/home/asic/arm-unknown-linux-gnueabi/bin/arm-unknown-linux-gnueabi-")
+
 gnu_arm_toolchain = generic_gcc_3_4_libgcc("arm-linux-")
-gnu_arm_nptl_toolchain = generic_gcc_3_4_libgcc("/opt/okl/Linux-i386/stow/arm-linux/bin/arm-linux-")
+
+#gnu_arm_nptl_toolchain = generic_gcc_3_4_libgcc("/opt/okl/Linux-i386/stow/arm-linux/bin/arm-linux-")
+gnu_arm_nptl_toolchain = generic_gcc_3_4_libgcc("/home/asic/opt/nicta/tools/gcc-3.4.4-glibc-2.3.5/arm-linux/bin/arm-linux-")
+
 gnu_mips64_toolchain = generic_gcc_3_4("mips64-elf-")
 gnu_mips32_toolchain = generic_gcc_3_4("mips-linux-")
-arm_linux_toolchain = generic_gcc_3_4("/opt/tools/3.4/arm-linux/bin/arm-linux-")
+
+#arm_linux_toolchain = generic_gcc_3_4("/opt/tools/3.4/arm-linux/bin/arm-linux-")
+arm_linux_toolchain = generic_gcc_3_4("/home/asic/opt/nicta/tools/gcc-3.4.4-glibc-2.3.5/arm-linux/bin/arm-linux-")
+
 gnu_ia32_toolchain = generic_gcc("i686-unknown-linux-gnu-")
 gnu_ia32_nptl_toolchain = generic_gcc("/opt/okl/Linux-i386/stow/i686-unknown-linux-gnu/bin/i686-unknown-linux-gnu-")
 
