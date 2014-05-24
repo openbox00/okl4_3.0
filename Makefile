f0f7d428f91a16c8667db1438c2576d9281306d4
openbox00 <openbox00.tw@gmail.com>
2014-05-24 15:02:42 +0800

    test soc_tracef

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 MACHINE=		versatile
 PATH := 	    /home/asic/opt/nicta/tools/gcc-3.4.4-glibc-2.3.5/arm-linux/bin/:$(PATH) 
 COMMAND=		./tools/build.py
-OPTIONS=		MACHINE=${MACHINE} wombat=true TOOLCHAIN=gnu_arm_eabi_toolchain pistachio.TOOLCHAIN=gnu_arm_toolchain PYFREEZE=False kdb_serial=True KDB_BREAKIN=False \
+OPTIONS=		MACHINE=${MACHINE} wombat=true TOOLCHAIN=gnu_arm_eabi_toolchain pistachio.TOOLCHAIN=gnu_arm_toolchain PYFREEZE=False kdb_serial=True KDB_BREAKIN=True \
 				pistachio.enter_kdb=true
 
 all: multicell
@@ -11,6 +11,9 @@ install:
 	echo $(PATH)
 multicell:
 	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=echo,chatterbox 
+
+2multicell:
+	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=oklinux,decrypt  LINUX_APPS=cross-cell-demo
 
 chatterbox:
 	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=chatterbox
@@ -24,8 +27,9 @@ echo:
 hello:
 	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=hello
 
+#linux_apps=lmbench
 oklinux:
-	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=oklinux linux_apps=lmbench
+	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=oklinux LINUX_APPS=cross-cell-demo 
 
 adder:
 	${COMMAND} ${OPTIONS} PROJECT=examples EXAMPLE=adder
--- libs/soc/include/interface.h
+++ libs/soc/include/interface.h
@@ -1,60 +1,3 @@
-/*
- * Copyright (c) 2007 Open Kernel Labs, Inc. (Copyright Holder).
- * All rights reserved.
- *
- * 1. Redistribution and use of OKL4 (Software) in source and binary
- * forms, with or without modification, are permitted provided that the
- * following conditions are met:
- *
- *     (a) Redistributions of source code must retain this clause 1
- *         (including paragraphs (a), (b) and (c)), clause 2 and clause 3
- *         (Licence Terms) and the above copyright notice.
- *
- *     (b) Redistributions in binary form must reproduce the above
- *         copyright notice and the Licence Terms in the documentation and/or
- *         other materials provided with the distribution.
- *
- *     (c) Redistributions in any form must be accompanied by information on
- *         how to obtain complete source code for:
- *        (i) the Software; and
- *        (ii) all accompanying software that uses (or is intended to
- *        use) the Software whether directly or indirectly.  Such source
- *        code must:
- *        (iii) either be included in the distribution or be available
- *        for no more than the cost of distribution plus a nominal fee;
- *        and
- *        (iv) be licensed by each relevant holder of copyright under
- *        either the Licence Terms (with an appropriate copyright notice)
- *        or the terms of a licence which is approved by the Open Source
- *        Initative.  For an executable file, "complete source code"
- *        means the source code for all modules it contains and includes
- *        associated build and other files reasonably required to produce
- *        the executable.
- *
- * 2. THIS SOFTWARE IS PROVIDED ``AS IS'' AND, TO THE EXTENT PERMITTED BY
- * LAW, ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED.  WHERE ANY WARRANTY IS
- * IMPLIED AND IS PREVENTED BY LAW FROM BEING DISCLAIMED THEN TO THE
- * EXTENT PERMISSIBLE BY LAW: (A) THE WARRANTY IS READ DOWN IN FAVOUR OF
- * THE COPYRIGHT HOLDER (AND, IN THE CASE OF A PARTICIPANT, THAT
- * PARTICIPANT) AND (B) ANY LIMITATIONS PERMITTED BY LAW (INCLUDING AS TO
- * THE EXTENT OF THE WARRANTY AND THE REMEDIES AVAILABLE IN THE EVENT OF
- * BREACH) ARE DEEMED PART OF THIS LICENCE IN A FORM MOST FAVOURABLE TO
- * THE COPYRIGHT HOLDER (AND, IN THE CASE OF A PARTICIPANT, THAT
- * PARTICIPANT). IN THE LICENCE TERMS, "PARTICIPANT" INCLUDES EVERY
- * PERSON WHO HAS CONTRIBUTED TO THE SOFTWARE OR WHO HAS BEEN INVOLVED IN
- * THE DISTRIBUTION OR DISSEMINATION OF THE SOFTWARE.
- *
- * 3. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ANY OTHER PARTICIPANT BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
- * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 /**
  * @file
  * @brief Functions provided by the general kernel that platform code may
@@ -335,9 +278,6 @@ void kernel_arch_init(void *param) NORETURN;
  * SOC code will be combeined with.  The SDK provides the list of defines
  * used for each kernel.o provided.
  */
-#if defined(CONFIG_DEBUG)
-#if defined(CONFIG_KDB_CONS)
-#if !defined(CONFIG_KDB_NO_ASSERTS)
 
 /**
  * @brief Access to the "core" kernel output.
@@ -347,9 +287,6 @@ void kernel_arch_init(void *param) NORETURN;
  */
 int kernel_printf(const char * format, ...);
 
-#if defined(_lint)
-#define SOC_ASSERT(level, x) do { if (! (x)) { __panic(); } } while(false)
-#else
 #define SOC_ASSERT(level, x)                                           \
     do {                                                                \
         if (EXPECT_FALSE(! (x)) && (level <= CONFIG_ASSERT_LEVEL)) {    \
@@ -358,7 +295,7 @@ int kernel_printf(const char * format, ...);
             kernel_enter_kdebug ("assert");                   \
         }                                                               \
     } while(false)
-#endif
+
 
 #define SOC_TRACEF(f, x...)                                            \
     do {                                                                \
@@ -367,29 +304,6 @@ int kernel_printf(const char * format, ...);
 
 #define SOC_TRACE(x...)    soc_printf(x)
 
-# else /* defined(CONFIG_KDB_NO_ASSERTS) */
-
-#define SOC_ASSERT(level, x)   do { } while (false)
-#define SOC_TRACE(x...)        do { } while (false)
-#define SOC_TRACEF(x...)       do { } while (false)
-
-#endif
-
-#else /* ! defined(CONFIG_KDB_CONS) */
-
-#define SOC_ASSERT(level, x)   do { } while (false)
-#define SOC_TRACE(x...)        do { } while (false)
-#define SOC_TRACEF(x...)       do { } while (false)
-
-#endif /* CONFIG_KDB_CONS */
-
-#else /* !defined(CONFIG_KDEBUG) */
-
-#define SOC_ASSERT(level, x)   do { } while (false)
-#define SOC_TRACE(x...)        do { } while (false)
-#define SOC_TRACEF(x...)       do { } while (false)
-
-#endif
 
 #ifdef __cplusplus
 } /* extern "C" */
